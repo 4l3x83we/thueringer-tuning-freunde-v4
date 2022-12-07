de28d44e42827d6652c0e9b7dba901e9cc2cbd38
Alexander Guthmann <4l3x83we.dev@gmail.com>
2022-12-07 16:00:28 +0100

    add Meta Header

---
+++ resources/views/layouts/partials/meta/meta.blade.php
@@ -0,0 +1,13 @@
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="{{ url()->full() }}">
+<meta property="og:title" content="@yield('title', 'Willkommen bei den Thüringer Tuning Freunden') | {{ config('app.name', 'Laravel') }}">
+<meta property="og:description" content="@yield('description', 'Marken offener Tuningclub Keine Markenbindung alle Fahrzeugtypen sind willkommen bei uns. Auch du musst nicht der Jugendliche sein Tuning macht auch im Alter Spaß.')">
+<meta property="og:image" content="@yield('images', '//www.thueringer-tuning-freunde.de/img/favicons/favicon-194x194.png')">
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="{{ url()->full() }}">
+<meta property="twitter:title" content="@yield('title', 'Willkommen bei den Thüringer Tuning Freunden') | {{ config('app.name', 'Laravel') }}">
+<meta property="twitter:description" content="@yield('description', 'Marken offener Tuningclub Keine Markenbindung alle Fahrzeugtypen sind willkommen bei uns. Auch du musst nicht der Jugendliche sein Tuning macht auch im Alter Spaß.')">
+<meta property="twitter:image" content="@yield('images', '//www.thueringer-tuning-freunde.de/img/favicons/favicon-194x194.png')">
