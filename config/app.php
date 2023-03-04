141e0a65a584d2e5c58e9854bd58977a772206d5
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-03-04 23:27:13 +0100

    config app fixed

--- config/app.php
+++ config/app.php
@@ -186,7 +186,6 @@ return [
          * Package Service Providers...
          */
         Intervention\Image\ImageServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -211,7 +210,6 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         'Image' => Intervention\Image\Facades\Image::class,
-        'Debugbar' => Barryvdh\Debugbar\Facades\Debugbar::class,
     ])->toArray(),
 
 ];
